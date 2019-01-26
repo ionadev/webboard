4e3d17cabbf1c85aa2551b783240db2c5dd89803
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-01-26 14:58:07 +0530

    dashboard

---
+++ precache-manifest.88c305641f6ccd8d7d36857e1b14a080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceb74b4f114a9b42ceeb265a9e466c16",
+    "url": "/static/media/logo.ceb74b4f.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e23a83161ccc7f3e4a92",
+    "url": "/static/js/main.e23a8316.chunk.js"
+  },
+  {
+    "revision": "0074c6030edfa0804389",
+    "url": "/static/js/1.0074c603.chunk.js"
+  },
+  {
+    "revision": "e23a83161ccc7f3e4a92",
+    "url": "/static/css/main.d88984fa.chunk.css"
+  },
+  {
+    "revision": "c2f28cfcd77b0d6835226be947503450",
+    "url": "/index.html"
+  }
+];
